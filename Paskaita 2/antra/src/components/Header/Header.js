4aab621e026564bda9446f03457b851b90d73627
PLIUSAS <Laurynas1297@gmail.com>
2023-10-10 21:28:51 +0300

    Paskaita 2

--- Paskaita 2/antra/src/components/Header/Header.js
+++ Paskaita 2/antra/src/components/Header/Header.js
@@ -4,15 +4,12 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2-qBFIHc9O_xk4AzMw4BtN253FJd95eu0QU30uYZcxS5V-x5ZR6jWt8Jsdsfkz1A8hag&usqp=CAU"
-        alt="logo"
-      />
-      <nav>
-        <a href="#Home">Home</a>
-        <a href="#Contacts">Contacts</a>
-        <a href="#Products">Products</a>
-      </nav>
+      <div>
+        <h1>Title</h1>
+      </div>
+      <h4>Some about text</h4>
+      <h4>in two lines</h4>
+      <button>Button</button>
     </header>
   );
 }
--- Paskaita 2/antra/src/components/Input/Input.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import style from "./Input.module.css";
-
-export default function input() {
-  return <div className={style.bacground}>Input</div>;
-}
